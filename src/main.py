88cb9cd79297e68a70ac6074f9030ed97591c35b
Tundzhay Mollov <graculapsone1998@gmail.com>
2022-01-25 21:16:08 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from PyQt5 import QtCore as qtc
 
 from src.components.folder_files_section.FavoriteFoldersTreeView import FavoriteFoldersTreeView
 from src.components.folder_files_section.FilesInFolderTable import FilesInFolderTable
+from src.components.folder_files_section.FolderAndFilesView import FolderAndFilesView
 from src.components.folder_files_section.FoldersTreeView import FoldersTreeView
 
 homeDir = "/home"
@@ -23,31 +24,49 @@ class MainWindow(qtw.QWidget):
         self.setLayout(layout_main)
 
         # Folders / Files section
-        layout_selection = qtw.QGridLayout()
-        view_folders = FoldersTreeView(userDir)
-        view_favorite_folders = FavoriteFoldersTreeView()
-
         data = {'Filename': ['Mascota', 'Miss Moniqueeeeeeeeeeeeeeeeeeeeeeeee', 'NCS', 'NCS Trap'],
                 'Size': ['59 123', '60 902', '4 504', '3 321'],
                 'Format': ['mp3', 'mp3', 'mp3', 'mp3'],
                 'Duration': ['1:11:13', '2:12:03', '4:54', '3:23']}
 
-        view_files_in_folder = FilesInFolderTable(data, 4, 4)
-        # Arrange views
-        layout_selection.addWidget(view_folders, 0, 0)
-        layout_selection.addWidget(view_favorite_folders, 1, 0)
-        layout_selection.addWidget(view_files_in_folder, 0, 1)
+        folder_and_file_section = FolderAndFilesView(userDir, data)
 
-        splitter1 = qtw.QSplitter(qtc.Qt.Vertical)
-        splitter1.addWidget(view_folders)
-        splitter1.addWidget(view_favorite_folders)
+        # Player section
+        hbox = qtw.QHBoxLayout()
 
-        splitter2 = qtw.QSplitter(qtc.Qt.Horizontal)
-        splitter2.addWidget(splitter1)
-        splitter2.addWidget(view_files_in_folder)
+        image = qtg.QPixmap("../test_img.jpg")
+        text_image = qtw.QLabel("image")
+        text_image.setPixmap(image)
 
-        # Adding Section to main layout
-        layout_main.addWidget(splitter2)
+        hbox.addWidget(text_image)
+
+        vert = qtw.QVBoxLayout()
+        vert1 = qtw.QVBoxLayout()
+        hbox1 = qtw.QHBoxLayout()
+        vert.addLayout(vert1)
+        vert.addLayout(hbox1)
+
+        text_name = qtw.QLabel("#FILENAME")
+        text_name.setAlignment(qtc.Qt.AlignLeft)
+
+        text_meta = qtw.QLabel("#METADATAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        text_meta.setAlignment(qtc.Qt.AlignLeft)
+        vert1.addWidget(text_name)
+        vert1.addWidget(text_meta)
+
+        text_time = qtw.QLabel("[ 11:11 / 22:22]")
+        text_time.setAlignment(qtc.Qt.AlignLeft)
+        repeat_btn = qtw.QPushButton("Repeat")
+        widg = qtw.QWidget()
+        hbox1.addWidget(text_time)
+        hbox1.addWidget(widg)
+        hbox1.addWidget(repeat_btn)
+
+        hbox.addLayout(vert)
+
+        # Adding Sections to main layout
+        layout_main.addWidget(folder_and_file_section)
+        layout_main.addLayout(hbox)
 
         self.show()
 
