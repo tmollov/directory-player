f59481168c7789ee403f9c227a94ff6803a3aaff
Tundzhay Mollov <graculapsone1998@gmail.com>
2022-02-01 23:05:49 +0200

    Updates in folders view and audio playback
    - Loading files to QMediaPlayer from folders view for now
    - Play / Pause button is working, but need updates, leaved TODO

--- src/main.py
+++ src/main.py
@@ -1,53 +1,77 @@
+import os
 import sys
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtCore as qtc
 from PyQt5 import Qt as qt
 
-from src.components.folder_files_section.FolderAndFilesView import FolderAndFilesView
-from src.components.folder_files_section.StandardItem import StandardItem
-from src.directories import Directories
-
 homeDir = "/home"
-userDir = homeDir + "/tmollov"
+userDir = homeDir + "/tmollov/Downloads"
 
 from src.dp_gui import Ui_MainWindow
+
 
 class MainWindow(qtw.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.show()
         self.setupUi(self)
+        self.audio_player = qt.QMediaPlayer()
+
+
+        # Model-View
+        self.file_system_model = qtw.QFileSystemModel()
+        self.file_system_model.setRootPath(userDir)
+        self.file_system_model.setNameFilters(["*.mp3"])
+        self.file_system_model.setNameFilterDisables(False)
+
+        # Setup views
         self.folders_setup()
 
     def folders_setup(self):
-        self.folders.setHeaderHidden(True)
-        tree_model = qt.QStandardItemModel()
-        root_node = tree_model.invisibleRootItem()
 
-        root_item = StandardItem(userDir)
+        # Folders props
+        self.folders.setModel(self.file_system_model)
+        self.folders.setRootIndex(self.file_system_model.index(userDir))
+        self.folders.hideColumn(2)
+        self.folders.hideColumn(3)
 
-        ls = Directories().get_standard_items(userDir)
+        # Signals
+        self.folders.clicked.connect(self.show_files)
+        self.button_play.clicked.connect(self.play_pause)
 
-        for item in ls:
-            root_item.appendRow(item)
+    # TODO: Implement auto button checked state switch when files are selected
+    def play_pause(self):
+        if(self.audio_player.isAvailable()):
+            if (self.button_play.isChecked() == True):
+                self.audio_player.play()
+                self.button_play.setText("Pause")
+            else:
+                self.audio_player.pause()
+                self.button_play.setText("Play")
 
-        root_node.appendRow(root_item)
 
-        self.folders.setModel(tree_model)
-        self.folders.expandAll()
 
+    def show_files(self, index):
+        indexItem = self.file_system_model.index(index.row(), 0, index.parent())
+        #fileName = self.file_system_model.fileName(indexItem)
+        filePath = self.file_system_model.filePath(indexItem)
+
+        #print(fileName)
+        print(filePath)
+
+        m = self.sender().model()
+        path = self.sender().model().filePath(index)
+
+        if(os.path.isfile(filePath)):
+            self.audio_player.setMedia(qt.QMediaContent(qt.QUrl.fromLocalFile(filePath)))
+            self.play_pause()
+
+        print(path)
+
+    # TODO
     def files_setup(self):
         pass
-
-    def old_init(self):
-        # Folders / Files section
-        data = {'Filename': ['Mascota', 'Miss Moniqueeeeeeeeeeeeeeeeeeeeeeeee', 'NCS', 'NCS Trap'],
-                'Size': ['59 123', '60 902', '4 504', '3 321'],
-                'Format': ['mp3', 'mp3', 'mp3', 'mp3'],
-                'Duration': ['1:11:13', '2:12:03', '4:54', '3:23']}
-
-        folder_and_file_section = FolderAndFilesView(userDir, data)
 
 
 if __name__ == "__main__":
