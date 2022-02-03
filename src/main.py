9dccf5d0d82daba80baef3890286d8257eef9d51
Tundzhay Mollov <graculapsone1998@gmail.com>
2022-02-04 00:34:16 +0200

    Update main.py
    Added
    - Logic for time slider
    - Logic for mute button

--- src/main.py
+++ src/main.py
@@ -39,26 +39,47 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
         self.button_repeat_track.clicked.connect(self.repeat_media)
         self.audio_player.mediaStatusChanged.connect(self.player_status_change)
         self.audio_player.positionChanged.connect(self.current_position)
+        self.audio_player.positionChanged.connect(self.update_slider)
+        self.time_slider.valueChanged.connect(self.change_position)
+        self.files.clicked.connect(self.play_file)
+        self.button_play.clicked.connect(self.play_pause)
+        self.button_mute.clicked.connect(self.mute_audio)
+
+    def mute_audio(self):
+        if self.button_mute.isChecked():
+            self.audio_player.setMuted(True)
+        else:
+            self.audio_player.setMuted(False)
+
+    def update_slider(self, val):
+        self.time_slider.setValue(val)
+
+    def change_position(self, val):
+        self.audio_player.setPosition(val)
 
     def current_position(self, position):
-        pos = self.convert_duration(position)
+        pos = self.convert_duration_to_str(position)
         self.player_time.setText(f'[{pos} / {self.duration}]')
 
     def player_status_change(self, status: qt.QMediaPlayer.MediaStatus):
-        print(status)
         if status == 6:
-            self.duration = self.convert_duration(self.audio_player.duration())
+            self.duration = self.convert_duration_to_str(self.audio_player.duration())
+            self.time_slider.setMaximum(self.audio_player.duration())
             self.player_time.setText(self.duration)
         if status == 7 and self.repeat_audio is True:
             self.audio_player.play()
 
-    def convert_duration(self, millis):
+    def convert_duration_to_str(self, millis):
         seconds = str(int((millis / 1000) % 60)).zfill(2)
         minutes = str(int((millis / (1000 * 60)) % 60)).zfill(2)
         hours = int((millis / (1000 * 60 * 60)) % 24)
         if hours < 1:
             return f'{minutes}:{seconds}'
         return f'{hours}:{minutes}:{seconds}'
+
+    def convert_duration_to_seconds(self, millis):
+        seconds = int(millis / 1000)
+        return seconds
 
     def play_pause(self):
         if self.button_play.isChecked():
@@ -69,13 +90,16 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
             self.button_play.setText("Play")
 
     def repeat_media(self):
-        self.repeat_audio = not self.repeat_audio
+        if not self.button_repeat_track.isChecked():
+            self.repeat_audio = False
+        else:
+            self.repeat_audio = True
 
     def play_file(self, index):
         file_path = self.get_file_path(index, self.files_sm)
         self.audio_player.setMedia(qt.QMediaContent(qt.QUrl.fromLocalFile(file_path)))
 
-        if self.button_play.isChecked() != True:
+        if not self.button_play.isChecked():
             self.button_play.click()
 
         self.player_filename.setText(self.get_file_name(index, self.files_sm))
@@ -84,7 +108,11 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
 
     def show_files(self, index):
         path = self.sender().model().filePath(index)
+
         self.files.setRootIndex(self.files_sm.setRootPath(path))
+        self.files_sm.setFilter(qt.QDir.Files)
+        self.files_sm.setNameFilters(["*.mp3"])
+        self.files_sm.setNameFilterDisables(False)
 
     def get_file_path(self, index, model):
         index_item = model.index(index.row(), 0, index.parent())
@@ -107,17 +135,9 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
 
         # Signals
         self.folders.clicked.connect(self.show_files)
-        self.files.clicked.connect(self.play_file)
-        self.button_play.clicked.connect(self.play_pause)
 
     def files_setup(self):
         self.files.setModel(self.files_sm)
-        select_mode = qt.QAbstractItemView.SelectionMode(qt.QAbstractItemView.SelectRows)
-        self.files.setSelectionMode(select_mode)
-
-        self.files.hideColumn(1)
-        self.files.hideColumn(2)
-        self.files.hideColumn(3)
 
 
 if __name__ == "__main__":
